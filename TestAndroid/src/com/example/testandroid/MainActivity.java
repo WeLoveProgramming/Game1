0bf7787e774d05d36e201b29fb621440a812b6fb
SocratesWong <socrateswonggg@gmail.com>
2014-08-29 16:47:55 -0700

    Push

--- TestAndroid/src/com/example/testandroid/MainActivity.java
+++ TestAndroid/src/com/example/testandroid/MainActivity.java
@@ -19,7 +19,10 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	public boolean onButtonClick(MenuItem item){
+		//android.R.id.Txt1="Troll";
+		return true;
+	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
